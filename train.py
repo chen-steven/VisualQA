eb92f16ea354bb3145393489629c02a5632caf97
Steven Chen <syc.stevenchen@gmail.com>
2020-08-10 11:59:01 -0400

    accuracy

--- train.py
+++ train.py
@@ -11,11 +11,14 @@ def train():
         optim.zero_grad()
         pred=model(q,q_len, f,obs)
         loss = compute_multi_loss(pred, a)
-        print(loss)
+       
         loss_item =  round(loss.item(),3)
         loss.backward()
         nn.utils.clip_grad_norm_(model.grad_params(), 5)
         optim.step()
+
+        acc = compute_multi_acc(pred, a)
+        print(acc)
 
 
 
@@ -23,5 +26,13 @@ def compute_multi_loss(logits, labels):
     loss = nn.functional.binary_cross_entropy_with_logits(logits, labels).to(labels.device)
     loss *= labels.size(1)
     return loss
+
+def compute_multi_acc(logits, labels):
+    logit_max = logits.max(dim = 1)[1] # argmax
+    logit_max.unsqueeze_(1).to(labels.device)
+    one_hots = torch.zeros(*labels.size()).to(labels.device)
+    one_hots.scatter_(1, logit_max, 1)
+    acc = (one_hots * labels).sum() / labels.size(0) * 100
+    return acc.item()
 
 train()
